bac819e47444833d15479b2ff4803c3ef80496bd
Devi prasad <prasad20697@gmail.com>
2024-04-23 18:04:51 +0530

    budget edit dialog

--- app/(main)/dashboard/expenses/_components/edit-budget.jsx
+++ app/(main)/dashboard/expenses/_components/edit-budget.jsx
@@ -10,12 +10,54 @@ import {
 	DialogTitle,
 	DialogTrigger,
 } from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import { PenBox } from "lucide-react";
+import EmojiPicker from "emoji-picker-react";
+import { Input } from "@/components/ui/input";
 
 const EditBudget = () => {
 	return (
 		<div>
-			<Dialog></Dialog>
-			Edit Budget
+			<Dialog>
+				<DialogTrigger asChild>
+					<Button className="flex gap-2">
+						<PenBox />
+						Edit
+					</Button>
+				</DialogTrigger>
+
+				<DialogContent>
+					<DialogHeader>
+						<DialogTitle>Update Budget</DialogTitle>
+						<DialogDescription>
+							<div className="mt-5">
+								<Button variant="outline" className="text-lg">
+									Emoji
+								</Button>
+
+								<EmojiPicker open={false} />
+							</div>
+							<div className="mt-2">
+								<h2 className="text-black font-medium my-1">
+									Budget Name
+								</h2>
+								<Input placeholder="eg, Home, Decor..," />
+							</div>
+							<div className="mt-2">
+								<h2 className="text-black font-medium my-1">
+									Budget Amount
+								</h2>
+								<Input type="number" placeholder="$ 500" />
+							</div>
+						</DialogDescription>
+					</DialogHeader>
+					<DialogFooter className="sm:justify-start">
+						<DialogClose asChild>
+							<Button disabled={false}>Update Budget</Button>
+						</DialogClose>
+					</DialogFooter>
+				</DialogContent>
+			</Dialog>
 		</div>
 	);
 };
