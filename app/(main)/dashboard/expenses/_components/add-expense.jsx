9e0f13fcd552791fb540bced1b11171bca13bd9b
Devi prasad <prasad20697@gmail.com>
2024-04-23 18:04:41 +0530

    add expense option

--- app/(main)/dashboard/expenses/_components/add-expense.jsx
+++ app/(main)/dashboard/expenses/_components/add-expense.jsx
@@ -1,7 +1,27 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Loader } from "lucide-react";
 import React from "react";
 
 const AddExpense = () => {
-	return <div>Add singular Expense</div>;
+	return (
+		<div className="border p-5 rounded-lg">
+			<h2 className="font-bold text-lg">Add Expense</h2>
+			<div className="mt-2">
+				<h2 className="text-black font-medium my-1">Name</h2>
+				<Input placeholder="ex, Bedroom decor" />
+			</div>
+
+			<div className="mt-2">
+				<h2 className="text-black font-medium my-1">Amount</h2>
+				<Input placeholder="e.g. 1000" />
+			</div>
+
+			<Button disabled={false} onClick={() => {}} className="mt-3 w-full">
+				{false ? <Loader className="animate-spin" /> : "Add New Expense"}
+			</Button>
+		</div>
+	);
 };
 
 export default AddExpense;
